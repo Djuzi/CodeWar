3d465f95596a421027ae389735961f185d83fab5
Your Name <you@example.com>
2020-08-02 13:07:27 +0300

    result kata

--- src/io/djuz/kata_simple_pig_latin/Main.java
+++ src/io/djuz/kata_simple_pig_latin/Main.java
@@ -1,8 +1,19 @@
 package io.djuz.kata_simple_pig_latin;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
+        //######################
+        //Третий вариант решения
+        //######################
+        var valStr = "Pig latin is cool";
+        String collect = Arrays.stream(valStr.trim().split(" "))
+                .map(v -> v.matches("[a-zA-Z]+") ? v.substring(1).concat(v.substring(0, 1)).concat("ay") : v)
+                .collect(Collectors.joining(" "));
+        System.out.println(collect);
 
         //######################
         //Второй вариант решения
@@ -17,19 +28,19 @@ public class Main {
 
 
         var inputParam = "Pig latin is cool".trim().split(" "); //=> igPay atinlay siay oolcay
-        StringBuilder stringBuilder=new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
 
         for (String value : inputParam) {
             if (Character.isAlphabetic(value.charAt(0))) {
                 char symbol = value.charAt(0);
                 String substring = value.substring(1);
                 stringBuilder.append(substring).append(symbol).append("ay").append(" ");
-            }else {
+            } else {
                 stringBuilder.append(value).append(" ");
             }
         }
-         String trimResult = stringBuilder.toString().trim();
-        System.out.println("Result->"+trimResult);
+        String trimResult = stringBuilder.toString().trim();
+        System.out.println("Result->" + trimResult);
 
     }
 }
